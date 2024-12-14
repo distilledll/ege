9e7273d713918ad6ca188e6d3ed8e83698b84393
distilledll <82288033+distilledll@users.noreply.github.com>
2024-12-14 16:38:37 +0300

    Update ex5.py

--- ex5.py
+++ ex5.py
@@ -1,1 +1,16 @@
-def n(a):
+def f(x):
+    res = ''
+    while x > 0:
+        res = str(x % 2) + res #заменить (2) на нужную сс
+        x //= 2
+    return res #string
+
+
+for n in range(1, 13): #по условию
+    x = f(n)
+    if n % 2 == 0:
+        x = '10' + x
+    else:
+        x = '1' + x + '01'
+    r = int(x, 2)
+    print(r)
